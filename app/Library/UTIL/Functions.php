d0df3c57078881a3a1b31f05e8d32be89b616c23
Manlio E Teran Ramos <manlioelnum1@hotmail.com>
2020-04-13 15:35:26 -0500

    mover file in functions

--- app/Library/UTIL/Functions.php
+++ app/Library/UTIL/Functions.php
@@ -405,6 +405,20 @@ class Functions
         fclose($ifp);
         return $output_file;
     }
+    public function mover_file(){
+        //almacenar/mover un archivo, la llave del arreglo $_file es 'file' $_FILES['file']
+
+        $target_path = "../../public/img/";
+        
+        $target_path = $target_path . basename( $_FILES['file']['name']);
+        
+        if(move_uploaded_file($_FILES['file']['tmp_name'], $target_path)) {
+            echo "Upload and move success";
+        } else{
+        echo $target_path;
+            echo "There was an error uploading the file, please try again!";
+        }
+    }
     public function text_only($text){
       //permite solo a-z0-9 y espacios
       $text = str_replace(
